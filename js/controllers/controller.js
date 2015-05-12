19305027c5ffa0be9c37f98165654a9d5ea30243
Vincent Vega <vincent@vega.lan>
2015-05-12 22:52:35 +0200

    code reorganization

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,52 +1,92 @@
 "use strict";
 var myApp = angular.module("regata", []);
 
-myApp.controller('homeController', ['$scope', function($scope) {
+myApp.controller('homeController', ['$scope',
+                                    '$window',
+                                    'contactGroundControl',
+                                    '$interval',
+                                    '$timeout', function($scope, $window, contactGroundControl, $interval, $timeout) {
 	$scope.statistics = {};
 	var perimeter = 0,
+	    prom,
 	    i,
 	    endIdx,
 	    len,
+	    dist,
 	    distanceArray = [];
 	$scope.gpsData = {};
-	for (i = 0; i < gpsArray.length; i++) {
-		endIdx = (i + 1)%gpsArray.length,
-		len = getDistanceFromLatLonInKm(gpsArray[i].lat,
-		                                    gpsArray[i].lng,
-		                                    gpsArray[endIdx].lat,
-		                                    gpsArray[endIdx].lng);
-		$scope.gpsData[gpsArray[i].name] = {
-			lat: gpsArray[i].lat,
-			lng: gpsArray[i].lng
-		};
-		perimeter += len;
-		distanceArray.push({
-			start: gpsArray[i].name,
-			end: gpsArray[endIdx].name,
-			lenKm: numberWithCommas(len.toFixed(3)),
-			lenM: numberWithCommas((len*1000).toFixed(2))
-		});
-		//console.log("Start: " + gpsArray[i].name +
-			    //"; End: " + gpsArray[endIdx].name +
-			    //"; Len: " + getGreatCircleDistance(gpsArray[i].lat,
-							       //gpsArray[i].lng,
-							       //gpsArray[endIdx].lat,
-							       //gpsArray[endIdx].lng));
+
+	$scope.updateStatistics = function() {
+		for (i = 0, len = dart.gpsArray.length; i < len; i++) {
+			endIdx = (i + 1)%dart.gpsArray.length,
+			dist = getDistanceFromLatLonInKm(dart.gpsArray[i].lat,
+											dart.gpsArray[i].lng,
+											dart.gpsArray[endIdx].lat,
+											dart.gpsArray[endIdx].lng);
+			$scope.gpsData[dart.gpsArray[i].name] = {
+				lat: dart.gpsArray[i].lat,
+				lng: dart.gpsArray[i].lng
+			};
+			perimeter += dist;
+			distanceArray.push({
+				start: dart.gpsArray[i].name,
+				end: dart.gpsArray[endIdx].name,
+				lenKm: numberWithCommas(dist.toFixed(3)),
+				lenM: numberWithCommas((dist*1000).toFixed(2))
+			});
+		}
+		$scope.statistics.perimeterKm = numberWithCommas(perimeter.toFixed(3));
+		$scope.statistics.perimeterM = numberWithCommas((perimeter*1000).toFixed(2));
+		$scope.statistics.distances = distanceArray;
 	}
-	$scope.statistics.perimeterKm = numberWithCommas(perimeter.toFixed(3));
-	$scope.statistics.perimeterM = numberWithCommas((perimeter*1000).toFixed(2));
-	$scope.statistics.distances = distanceArray;
 
 	$scope.drawPath = function(elem, start, end) {
 		if (elem .present === false ) {
-			drawLine($scope.gpsData[start].lat, $scope.gpsData[start].lng,
+			dart.drawLine($scope.gpsData[start].lat, $scope.gpsData[start].lng,
 			         $scope.gpsData[end].lat, $scope.gpsData[end].lng,
 			         start + end);
 			elem.present = true;
 		} else {
-			clearLine(start + end);
+			dart.clearLine(start + end);
 			elem.present = false;
 		}
+	};
+	prom = $interval(function() { contactGroundControl(); $scope.updateStatistics; }, 10000);
+	//$interval.cancel(prom);
+	$window.onload = function () {
+		contactGroundControl();
+		$timeout($scope.updateStatistics, 2000);
+	}
+}]);
+
+myApp.factory('contactGroundControl', [function() {
+	return function() {
+		dart.gpsArray = [
+			{
+				name: "Gps-1",
+				type: "buoy",
+				lat: 32.199904, lng: -78.783251
+			},{
+				name: "Gps-2",
+				type: "buoy",
+				lat: 29.6880527498568, lng: -87.802734375
+			},{
+				name: "Gps-3",
+				type: "buoy",
+				lat: 22.06527806776582, lng: -87.8466796875
+			},{
+				name: "Jury-Boat",
+				type: "boat",
+				lat: 25.165173368663954, lng: -76.552734375
+			}
+		];
+		dart.clearMarkers();
+		// Initialize Markers
+		for (var i = 0, len = dart.gpsArray.length; i < len; i++) {
+			dart.addMarker(new google.maps.LatLng(dart.gpsArray[i].lat,
+			                                      dart.gpsArray[i].lng), dart.gpsArray[i]);
+		}
+		dart.fitMapZoom();
 	};
 }]);
 
--- js/maps.js
+++ js/maps.js
@@ -1,36 +1,84 @@
 "use strict";
-var map = map || {},
-    markers = markers || [],
-    polyLines = polyLines || {},
-
-gpsArray = [
-	{
-		name: "Gps-1",
-		type: "buoy",
-		lat: 32.199904, lng: -78.783251
-	},{
-		name: "Gps-2",
-		type: "buoy",
-		lat: 29.6880527498568, lng: -87.802734375
-	},{
-		name: "Gps-3",
-		type: "buoy",
-		lat: 22.06527806776582, lng: -87.8466796875
-	},{
-		name: "Jury-Boat",
-		type: "boat",
-		lat: 25.165173368663954, lng: -76.552734375
-	}
-],
-
-iconStyle = {
-	"boat": {
-		url: "img/motor-boat-min.png",
-		position: new google.maps.Point(15, 15)
+var dart = dart || {
+	map: {},
+	markers: [],
+	polyLines: {},
+	gpsArray: [],
+	iconStyle: {
+		"boat": {
+			url: "img/motor-boat-min.png",
+			position: new google.maps.Point(15, 15)
+		},
+		"buoy": {
+			url:"img/map-marker-20.png",
+			position: new google.maps.Point(10, 25)
+		}
 	},
-	"buoy": {
-		url:"img/map-marker-20.png",
-		position: new google.maps.Point(10, 25)
+
+	fitMapZoom: function() {
+		var bounds = new google.maps.LatLngBounds(),
+			i,
+			len = this.gpsArray.length;
+		if (len > 0) {
+			for (i = 0; i < len; i++) {
+				bounds.extend(new google.maps.LatLng(this.gpsArray[i].lat,
+				                                     this.gpsArray[i].lng));
+			}
+			this.map.fitBounds(bounds);
+			this.map.panToBounds(bounds);
+		}
+	},
+
+	addMarker: function(position, elem) {
+		var markerIcon = {
+			url: this.iconStyle[elem.type]["url"],
+			anchor: this.iconStyle[elem.type]["position"]
+		}
+		this.markers.push(new google.maps.Marker({
+			position: position,
+			title: elem.name,
+			map: this.map,
+			icon: markerIcon,
+			animation: google.maps.Animation.DROP
+		}));
+	},
+
+	clearMarkers: function() {
+		for (var i = 0, len = this.markers.length; i < len; i++) {
+			this.markers[i].setMap(null);
+		}
+		this.markers = [];
+	},
+
+	clearLines: function() {
+		for (var key in this.polyLines) {
+			this.polyLines[key].setMap(null);
+		}
+		this.polyLines = {};
+	},
+
+	drawLine: function(lat1, lng1, lat2, lng2, label) {
+		if (label in this.polyLines) {
+			this.polyLines[label].setMap(null);
+		}
+		var line = new google.maps.Polyline({
+			path: [
+				new google.maps.LatLng(lat1, lng1),
+				new google.maps.LatLng(lat2, lng2)
+			],
+			geodesic: true,
+			strokeColor: '#FF0000',
+			strokeOpacity: 1.0,
+			strokeWeight: 3
+		});
+		line.setMap(this.map);
+		this.polyLines[label] = line;
+	},
+
+	clearLine: function(label) {
+		if (this.polyLines[label]) {
+			this.polyLines[label].setMap(null);
+		}
 	}
 };
 
@@ -63,104 +111,23 @@ function init() {
 	}
 
 	var mapOptions = {
+		center: { lat: 44.01148674722159, lng: 10.102918446063995 },
+		zoom: 12,
 		mapTypeControlOptions: {
 			//mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
 			//mapTypeIds: [google.maps.MapTypeId.TERRAIN, 'map_style']
 			//mapTypeIds: [google.maps.MapTypeId.SATELLITE, 'map_style']
 			mapTypeIds: [google.maps.MapTypeId.HYBRID, 'map_style']
 		}
-	},
-	    bounds = zoomControl();
-	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-	map.mapTypes.set('map_style', initStyledMap());
-	//map.setMapTypeId('map_style');
-	map.fitBounds(bounds);
-	map.panToBounds(bounds);
+	};
+	dart.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	dart.map.mapTypes.set('map_style', initStyledMap());
+	//dart.map.setMapTypeId('map_style');
 
 	// XXX debug
-	google.maps.event.addListener(map, "rightclick", function(event) {
+	google.maps.event.addListener(dart.map, "rightclick", function(event) {
 		alert("lat: " + event.latLng.lat() + ", lng: " + event.latLng.lng());
 	});
-
-	// Initialize Markers
-	(function () {
-		var i,
-		    len,
-		    lat,
-		    lng;
-		clearMarkers();
-		for (i = 0, len = gpsArray.length; i < len; i++) {
-			lat = gpsArray[i].lat,
-			lng = gpsArray[i].lng;
-			//console.log("i: " + i + " Lat: " + lat + "; Long: " + lng); // XXX debug
-			addMarker(new google.maps.LatLng(lat, lng), gpsArray[i]);
-		}
-	}());
-
-	function addMarker(position, elem) {
-		var markerIcon = {
-			url: iconStyle[elem.type]["url"],
-			anchor: iconStyle[elem.type]["position"]
-		}
-		markers.push(new google.maps.Marker({
-			position: position,
-			title: elem.name,
-			map: map,
-			icon: markerIcon,
-			animation: google.maps.Animation.DROP
-		}));
-	}
-
-	function clearMarkers() {
-		for (var i = 0, len = markers.length; i < len; i++) {
-			markers[i].setMap(null);
-		}
-		markers = [];
-	}
-}
-
-function zoomControl() {
-	var bounds = new google.maps.LatLngBounds(),
-	    i,
-	    len;
-	for (i = 0, len = gpsArray.length; i < len; i++) {
-		bounds.extend(new google.maps.LatLng(gpsArray[i].lat, gpsArray[i].lng));
-	}
-	return bounds;
-}
-
-function drawLine(lat1, lng1, lat2, lng2, label) {
-	console.log(lat1 + "," + lng1 + "," + lat2 + "," + lng2 + "," + label);
-	if (label in polyLines) {
-		polyLines[label].setMap(null);
-	}
-	var linePath = [
-		new google.maps.LatLng(lat1, lng1),
-		new google.maps.LatLng(lat2, lng2)
-	],
-
-	line = new google.maps.Polyline({
-		path: linePath,
-		geodesic: true,
-		strokeColor: '#FF0000',
-		strokeOpacity: 1.0,
-		strokeWeight: 3
-	});
-	line.setMap(map);
-	polyLines[label] = line;
-}
-
-function clearLine(label) {
-	if (polyLines[label]) {
-		polyLines[label].setMap(null);
-	}
-}
-
-function clearLines() {
-	for (var key in polyLines) {
-		polyLines[key].setMap(null);
-	}
-	polyLines = {};
 }
 
 google.maps.event.addDomListener(window, 'load', init);
